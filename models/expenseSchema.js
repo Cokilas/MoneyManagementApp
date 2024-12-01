002449b852dd008c31d0170898270f37cfb71bcc
Chris Bentley <chrisrbentley@pm.me>
2024-12-01 11:48:21 -0500

    Updated schema and added gitignore

--- models/expenseSchema.js
+++ models/expenseSchema.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
 const ExpenseSchema = new mongoose.Schema({
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}, // Ref to the user
-    budgetId: {type: mongoose.Schema.Types.ObjectId, ref: 'Budget', required: true}, // Ref to budget
-    name: {type: String, required: true}, // Name of expense
-    amount: {type: String, required: true}, // Amount spent
-    date: {type: Date, default: Date.now},
+	userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Ref to the user
+	budgetId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Budget',
+		required: true,
+	}, // Ref to budget
+	name: { type: String, required: true }, // Name of expense
+	amount: { type: Number, required: true }, // Amount spent
+	date: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Expense', ExpenseSchema);
